9c5f87f2c451ed1fed533bc4e21f82c2fc5a3e99
voximir-p <pek795b@gmail.com>
2026-04-09 17:23:59 +0700

    fix: can't find 'render' method

--- src/main/java/org/voximir/client_recipe_fix/client/mixin/ImportantWarningsWidgetMixin.java
+++ src/main/java/org/voximir/client_recipe_fix/client/mixin/ImportantWarningsWidgetMixin.java
@@ -1,12 +1,13 @@
 package org.voximir.client_recipe_fix.client.mixin;
 
+import me.shedaniel.rei.impl.client.gui.hints.ImportantWarningsWidget;
 import net.minecraft.client.gui.GuiGraphics;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
-@Mixin(targets = "me.shedaniel.rei.impl.client.gui.hints.ImportantWarningsWidget")
+@Mixin(ImportantWarningsWidget.class)
 public class ImportantWarningsWidgetMixin {
 
     @Inject(method = "render", at = @At("HEAD"), cancellable = true)
