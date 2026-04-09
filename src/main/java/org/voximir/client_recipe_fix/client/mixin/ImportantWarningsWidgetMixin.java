09c4346b78bd16e0206efe4ebbd1608726b6eb00
voximir-p <pek795b@gmail.com>
2026-04-09 18:52:14 +0700

    fix: add REI as modImplementation and modify mixin

--- src/main/java/org/voximir/client_recipe_fix/client/mixin/ImportantWarningsWidgetMixin.java
+++ src/main/java/org/voximir/client_recipe_fix/client/mixin/ImportantWarningsWidgetMixin.java
@@ -1,15 +1,16 @@
 package org.voximir.client_recipe_fix.client.mixin;
 
+import me.shedaniel.rei.impl.client.gui.hints.ImportantWarningsWidget;
 import net.minecraft.client.gui.GuiGraphics;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
-@Mixin(targets = "me.shedaniel.rei.impl.client.gui.hints.ImportantWarningsWidget")
+@Mixin(ImportantWarningsWidget.class)
 public class ImportantWarningsWidgetMixin {
 
-    @Inject(method = "method_25394(Lnet/minecraft/class_332;IIF)V", at = @At("HEAD"), cancellable = true, remap = false)
+    @Inject(method = "render", at = @At("HEAD"), cancellable = true)
     private void cancelRender(GuiGraphics graphics, int mouseX, int mouseY, float delta, CallbackInfo ci) {
         ci.cancel();
     }
