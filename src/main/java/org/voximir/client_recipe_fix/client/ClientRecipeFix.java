87e1c48a670a10e8e18bbdd913a68a5820fbc25f
DustinTrumps <dustintrumps@putlook.com>
2026-07-11 22:45:58 -0700

    works now

--- src/main/java/org/voximir/client_recipe_fix/client/ClientRecipeFix.java
+++ src/main/java/org/voximir/client_recipe_fix/client/ClientRecipeFix.java
@@ -5,11 +5,18 @@ import net.fabricmc.loader.api.FabricLoader;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import net.minecraft.world.item.crafting.RecipeHolder;
+
 public class ClientRecipeFix implements ClientModInitializer {
     public static final Logger LOGGER = LoggerFactory.getLogger("Client Recipe Fix");
 
     public static final boolean jeiLoaded = FabricLoader.getInstance().isModLoaded("jei");
     public static final boolean reiLoaded = FabricLoader.getInstance().isModLoaded("roughlyenoughitems");
+
+    public static List<RecipeHolder<?>> preloadedRecipes = new ArrayList<>();
 
     @Override
     public void onInitializeClient() {
